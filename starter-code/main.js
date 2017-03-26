5c2d9763d7e883c678df2deb903c9c47181bbc6b
Robert Quant <Robert@Zombies-MacBook-Pro.local>
2017-03-26 16:34:35 -0400

    added divs

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,5 @@
 // console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var gameCards = ["queen", "queen", "king", "king"];
 var gameBoard = "gb";
 /*if (cardOne === cardTwo) {
   window.alert("You found a match!");
@@ -16,25 +13,9 @@ var gameBoard = "gb";
   // Code to be executed if condition1, condition2, and condition3 are false
 }
 */
-var card = document.getElementById(gameBoard)
-var card = function (num) {
-    for (var i = 1; i <= num; i += 1) {
-        if (cardOne === cardTwo) {
-            window.alert("You found a match!");
-        }
-        else if (cardOne === cardThree) {
-            window.alert("Sorry, try again.")
-        }
-        else if (cardThree === cardFour) {
-            window.alert("You found a match!")
-        }
-        else if (cardThree === cardOne) {
-            window.alert("Sorry, Try again.")
-        }
-        else {
-            // Code to be executed if condition1, condition2, and condition3 are false
-        }
-        console.log(result);
-    }
-};
-document.createCards(gameBoard)
+var gameBoard = document.getElementById("game-board");
+for(var x = 0; x < gameCards.length; x++){
+    var card = document.createElement("div");
+    card.classList.add("card");
+    gameBoard.appendChild(card);
+}
